38737721b3a126476317e1a123c815003db05019
malhotrasarika789-blip <malhotrasarika789@gmail.com>
2026-07-28 11:13:58 +0530

    Fix routes import and CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+
 import authRoutes from "./routes/auth.js";
 import pollRoutes from "./routes/poll.js";
 import responseRoutes from "./routes/response.js";
@@ -8,21 +9,21 @@ import userRoutes from "./routes/user.routes.js";
 const app = express();
 
 app.use(cors({
-origin:[
-"http://localhost:5173",
-"https://frontend-psi-indol-95.vercel.app",
-"https://frontend-4poa466z0-malhotrasarika789-1336s-projects.vercel.app"
-],
-methods:["GET","POST","PATCH","DELETE"],
-credentials:true
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-psi-indol-95.vercel.app",
+    "https://frontend-4poa466z0-malhotrasarika789-1336s-projects.vercel.app",
+    "https://frontend-fbgn76xb8-malhotrasarika789-1336s-projects.vercel.app"
+  ],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true
 }));
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/polls", pollRoutes);
-app.use("/api/responses",responseRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/responses", responseRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
-
